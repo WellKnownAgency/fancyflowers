3d60bd0e26c62f129b562abfecf5c2d901d7286a
Taras <taraskim@wknown.com>
2018-08-17 13:28:58 -0400

    Added Index page

---
+++ resources/views/main.blade.php
@@ -0,0 +1,10 @@
+<html lang-"en">
+  <head>
+    @include('partials/_head')
+  </head>
+  <body class="index home-1">
+    <div id="all">
+      <
+    </div>
+  </body>
+</html>
---
+++ resources/views/partials/_head.blade.php
@@ -0,0 +1,41 @@
+  <!-- Basic Page Needs -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>byHands Responsive HTML Template</title>
+
+  <meta name="keywords" content="Responsive HTML Template">
+  <meta name="description" content="Scara Responsive HTML Template">
+  <meta name="author" content="tivatheme">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+
+  <!-- Mobile Meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <!-- Google Fonts -->
+  <link href='https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700' rel='stylesheet' type='text/css'>
+
+  <!-- Vendor CSS -->
+  <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap-theme.css">
+  <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="vendor/font-material/css/material-design-iconic-font.min.css">
+  <link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.css">
+  <link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.css">
+
+  <link rel="stylesheet" href="vendor/nivo-slider/css/nivo-slider.css">
+  <link rel="stylesheet" href="vendor/nivo-slider/css/animate.css">
+  <link rel="stylesheet" href="vendor/nivo-slider/css/style.css">
+
+  <link rel="stylesheet" href="vendor/slider-range/css/jslider.css">
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="css/theme-global.css">
+  <link rel="stylesheet" href="css/theme-animate.css">
+  <link rel="stylesheet" href="css/theme-product.css">
+  <link rel="stylesheet" href="css/theme-product-list.css">
+  <link rel="stylesheet" href="css/theme-blog.css">
+  <link rel="stylesheet" href="css/theme-responsive.css">
+
+  <!-- Template Custom CSS -->
+  <link rel="stylesheet" href="css/custom.css">
