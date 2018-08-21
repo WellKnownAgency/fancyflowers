f91172a79cc50542d3445bb2c5e4083fd6cb2dd1
Taras <taraskim@wknown.com>
2018-08-21 11:49:40 -0400

    Changed Menu and Home Page

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -2,23 +2,16 @@
   <div class="footer-center">
     <div class="container">
       <div class="row">
-        <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 col-sp-12">
-          <div class="block-keep block">
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 col-sp-12">
+          <div class="contact-box block">
             <h4 class="title_block">About us</h4>
-            <div class="block_content">
-              <p>Lorem Khaled Ipsum is a major key to success. To be successful you’ve got to work hard, to make history, simple, you’ve got to make it. You do know</p>
-            </div>
-          </div>
-          <div class="social_block">
-            <ul class="links">
-              <li><a href="#"><em class="fa fa-facebook"></em><span class="unvisible">facebook</span> </a></li>
-              <li><a href="#"><em class="fa fa-twitter"></em><span class="unvisible">twitter</span> </a></li>
-              <li><a href="#"><em class="fa fa-google-plus"></em><span class="unvisible">google</span> </a></li>
-              <li><a href="#"><em class="fa fa-linkedin"></em><span class="unvisible">linkedin</span> </a></li>
-              <li><a href="#"><em class="fa fa-pinterest"></em><span class="unvisible">pinterest</span> </a></li>
-              <li class="last"><a href="#"><em class="fa fa-instagram"></em><span class="unvisible">instagram</span> </a></li>
+            <p>Lorem Khaled Ipsum is a major key to success. To be successful you’ve got to work hard, to make history, simple, you’ve got to make it.<p>
+            <ul>
+              <li><i class="zmdi zmdi-pin"></i><span>123 canberra Street, New York, USA</span></li>
+              <li><i class="zmdi zmdi-phone"></i><span>+844 123 456 78 / +844 123 456 79</span></li>
+              <li><i class="zmdi zmdi-email"></i><a href="#" title="">contact@byhands.org</a></li>
             </ul>
-          </div><!-- end social_block -->
+          </div><!-- contact-box-->
         </div>
         <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12 col-sp-12">
           <div class="footer-block block">
@@ -44,15 +37,30 @@
             </ul>
           </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 col-sp-12">
-          <div class="block-html block">
-            <h4 class="title_block">Opening time</h4>
-            <div class="block_content">
-              <p>Monday to Friday ....... 8 am - 10 pm</p>
-              <p>Saturday & Sunday ....... 10 am - 9 pm</p>
-              <p>Holiday is off</p>
-            </div>
-          </div><!-- end block-gallery -->
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 col-sp-12">
+          <div class="block-newsletter block">
+            <h4 class="title_block">Newsletter</h4>
+            <form action="#" method="post" class="form-inline">
+              <div class="form-group">
+                <input class="inputNew form-control grey newsletter-input" id="newsletter-input" type="text" name="email" size="18" placeholder="Enter your email...">
+                <button type="submit" name="submitNewsletter" class="btn-primary btn button button-small">
+                  <span class="submit-icon"></span>
+                  <span class="submit-text">Subscribe</span>
+                </button>
+              </div>
+            </form>
+          </div><!-- end block-newsletter -->
+          <div class="social_block block">
+            <h4 class="title_block">Follow us</h4>
+            <ul class="links">
+              <li><a href="#"><em class="fa fa-facebook"></em><span class="unvisible">facebook</span> </a></li>
+              <li><a href="#"><em class="fa fa-twitter"></em><span class="unvisible">twitter</span> </a></li>
+              <li><a href="#"><em class="fa fa-google-plus"></em><span class="unvisible">google</span> </a></li>
+              <li><a href="#"><em class="fa fa-linkedin"></em><span class="unvisible">linkedin</span> </a></li>
+              <li><a href="#"><em class="fa fa-pinterest"></em><span class="unvisible">pinterest</span> </a></li>
+              <li class="last"><a href="#"><em class="fa fa-instagram"></em><span class="unvisible">instagram</span> </a></li>
+            </ul>
+          </div><!-- end social_block -->
         </div>
       </div><!-- end row -->
     </div>
@@ -61,8 +69,13 @@
   <div class="footer-copyright">
     <div class="container">
       <div class="row">
-        <div class="text-center col-lg-12 col-md-12 col-sm-12 col-xs-12 col-sp-12">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-sp-12">
           Copyright © 2017 - All rights reserved. Powered by Tivatheme
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-sp-12">
+          <div class="payment">
+            <img class="img-responsive" src="img/banner/payment.png" alt="">
+          </div>
         </div>
       </div>
     </div>
