5b0bb19117f7be99298829528d8e69bcb8d4f3c2
Taras <taraskim@wknown.com>
2018-08-21 16:38:20 -0400

    Added Test Images and Logo
    Also fixed home page

---
+++ resources/views/partials/_slideshow.blade.php
@@ -0,0 +1,23 @@
+<div class="tiva-slideshow-wrapper">
+  <div id="tiva-slideshow" class="nivoSlider">
+    <a href="#" title="Slideshow image"><img class="img-responsive" src="images/slide/slide1-h1.jpg" title="#caption1" alt="Slideshow image"></a>
+    <a href="#" title="Slideshow image"><img class="img-responsive" src="images/slide/slide2-h1.jpg" title="#caption2" alt="Slideshow image"></a>
+  </div>
+
+  <div id="caption1" class="nivo-html-caption">
+    <div class="tiva-caption">
+      <div class="left-right hidden-xs medium_yellow_16"><span>Accessories</span></div>
+      <div class="left-right hidden-xs normal very_large_60">Floral headband</div>
+      <div class="left-right  hidden-md hidden-sm hidden-xs slow medium_16">when an unknown printer took a galley of type and scrambled it to <br>make a type specimen book. It has survived not only five centuries, <br>but also the leap into electronic typesetting, remaining.</div>
+      <div class="left-right hidden-xs slow"><a class="btn button btn-now" href="#" title="Shop now">Shop now</a></div>
+    </div>
+  </div>
+  <div id="caption2" class="nivo-html-caption">
+    <div class="tiva-caption">
+      <div class="left-right hidden-xs medium_yellow_16"><span>Accessories</span></div>
+      <div class="left-right hidden-xs normal very_large_60">Floral headband</div>
+      <div class="left-right  hidden-md hidden-sm hidden-xs slow medium_16">when an unknown printer took a galley of type and scrambled it to <br>make a type specimen book. It has survived not only five centuries, <br>but also the leap into electronic typesetting, remaining.</div>
+      <div class="left-right hidden-xs slow"><a class="btn button btn-now" href="#" title="Shop now">Shop now</a></div>
+    </div>
+  </div>
+</div><!-- end tiva-slideshow-wrapper -->
