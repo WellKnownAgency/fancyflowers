a12475cf1b53ad0d8b816d0f5065b52537b259ee
Taras <taraskim@wknown.com>
2018-08-22 17:41:45 -0400

    Added Admin
    Added Admin Front Page.
    Created FlowerController and Model
    Created Flower Table for Database

---
+++ database/migrations/2018_08_22_174214_create_flowers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFlowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flowers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('price');
+            $table->string('rating');
+            $table->string('dscr');
+            $table->string('quantity');
+            $table->boolean('stock')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flowers');
+    }
+}
