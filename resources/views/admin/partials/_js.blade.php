5c4d81534b9503ceadd490222f4c30798b519391
Taras <taraskim@wknown.com>
2018-08-22 17:41:45 -0400

    Added Admin
    Added Admin Front Page.
    Created FlowerController and Model
    Created Flower Table for Database

---
+++ resources/views/admin/partials/_js.blade.php
@@ -0,0 +1,3 @@
+<script src="{!! asset('js/app.js') !!}"></script>
+<script src="{!! asset('vendor/jquery/jquery.min.js') !!}"></script>
+<script src="{!! asset('vendor/bootstrap/js/bootstrap.min.js') !!}"></script>
