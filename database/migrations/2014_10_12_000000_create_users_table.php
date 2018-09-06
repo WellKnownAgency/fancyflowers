9af07355291d393a31a95fd6792073645d6c01d0
Taras <taraskim@wknown.com>
2018-09-06 11:59:52 -0400

    Migrations Fixes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('firstname');
-            $table->string('lasttname');
+            $table->string('lastname');
             $table->string('phonenumber');
             $table->string('email')->unique();
             $table->string('password');
