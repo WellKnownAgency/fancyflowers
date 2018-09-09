5738ee7f49e205a9a9e289e8c3f70882827cc22a
anton <anton.likhnekevich@gmail.com>
2018-09-09 14:48:09 -0400

    Account Payment Methods upgrade
    - create a payment method
    - edit card information

---
+++ resources/views/cards/edit.blade.php
@@ -0,0 +1,64 @@
+@section('title', '')
+@section('dscr', '')
+@section('keywords', '')
+
+@extends('main')
+
+@section('content')
+<div id="breadcrumb" class="clearfix">
+  <div class="container">
+    <div class="breadcrumb clearfix">
+      <ul class="ul-breadcrumb">
+        <li><a href="index.html" title="Home">Home</a></li>
+        <li><a href="index.html" title="Home">Account</a></li>
+      </ul>
+      <h2 class="bread-title">Edit a Card Information</h2>
+    </div>
+  </div>
+</div><!-- end breadcrumb -->
+  <div class="container">
+    <div class="row">
+      <div class=" col-md-8 col-md-offset-2">
+          <form action="{{ action('CardController@update', $card->id) }}" id="form-account-creation" class="form-horizontal box panel panel-default" method="POST">
+            @csrf
+            <h3 class="panel-heading">Edit {{$card->name}} Information </h3>
+
+            <div class="form_content panel-body clearfix">
+              <div class="form-group required">
+                <div class="col-lg-12">
+                  <label for="name">Name on card<sup>*</sup></label>
+                  <input type="text" class="form-control" id="name" name="name" value="{{$card->name}}" required>
+                </div>
+              </div>
+              <div class="form-group required">
+                <div class="col-lg-12">
+                  <label for="number">Number<sup>*</sup></label>
+                  <input type="text" class="form-control" id="number" name="number" value="{{$card->number}}" required>
+                </div>
+              </div>
+              <div class="form-group required">
+                <div class="col-lg-12">
+                  <label for="month">Last Name<sup>*</sup></label>
+                  <input type="text" class="form-control" id="month" name="month" value="{{$card->month}}" required>
+                </div>
+              </div>
+              <div class="form-group required">
+                <div class="col-lg-12">
+                  <label for="year">Street Address <sup>*</sup></label>
+                  <input type="text" class="form-control" id="year" name="year" value="{{$card->year}}" required>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-lg-12">
+                  <button type="submit" class="btn button btn-default">Save</button>
+                  <p class="pull-right required"><span><sup>*</sup>Required field</span></p>
+                </div>
+              </div>
+            </div>
+          </form><!--end form -->
+        </div>
+      </div>
+  </div> <!-- end container -->
+</br>
+
+@stop
