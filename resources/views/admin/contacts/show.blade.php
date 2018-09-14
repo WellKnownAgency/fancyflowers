c55528bba4361a71feff10727962142b31e465f2
anton <anton.likhnekevich@gmail.com>
2018-09-14 11:02:47 -0400

    Contact Form

---
+++ resources/views/admin/contacts/show.blade.php
@@ -0,0 +1,49 @@
+@section('title', '')
+@section('dscr', '')
+@section('keywords', '')
+
+@extends('admin.main')
+
+@section('content')
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-3">
+      <div class="card" style="width: 100%;">
+        <div class="card-header">
+          Fancy Flowers Contacts
+        </div>
+        <div class="card-body">
+          <a href="/admin/contacts" class="btn btn-info">All Contacts</a>
+          <br><br>
+          <a href="/admin/contacts/" class="btn btn-danger">Delete Contact</a>
+        </div>
+      </div>
+      <br>
+      <div class="card" style="width: 100%;">
+        <div class="card-header">
+          Date
+        </div>
+        <div class="card-body">
+          <strong>Created:</strong> {{ $contact->created_at }}<br>
+          <strong>Updated:</strong> {{ $contact->updated_at }}
+        </div>
+      </div>
+    </div>
+    <div class="col-md-9">
+      <div class="card" style="width: 100%;">
+        <h2 class="card-header">
+         {{ $contact->name }}
+
+       </h2>
+
+       <ul class="list-group list-group-flush">
+         <li class="list-group-item">{{ $contact->name }}</li>
+         <li class="list-group-item">{{ $contact->email }}</li>
+         <li class="list-group-item">{{ $contact->subj }}</li>
+         <li class="list-group-item">{{ $contact->body }}</li>
+       </ul>
+      </div>
+    </div>
+  </div>
+</div>
+@stop
