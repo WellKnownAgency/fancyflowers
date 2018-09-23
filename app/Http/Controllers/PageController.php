f58925a043943f10b10cc6df990dd82844b4349a
Taras <taraskim@wknown.com>
2018-09-23 16:29:54 -0400

    initial commit

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use App\Flower;
-use Vinkla\Instagram\Instagram;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -16,10 +15,9 @@ class PageController extends Controller
      */
     public function index()
     {
-      $instagram = new Instagram('248274392.1677ed0.8a29b5a11e5f4e0f8b8e6ddd0933ac4c');
       $flowers = Flower::latest()->limit(8)->get();
       $posts = Post::latest()->limit(5)->get();
-      return view('index', compact('flowers'))->withPosts($posts)->withInstagram($instagram);
+      return view('index', compact('flowers'))->withPosts($posts);
     }
 
 }
