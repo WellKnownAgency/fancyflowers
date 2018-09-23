560a922344a9d67cb43f6433879118c61d4d271b
Taras <taraskim@wknown.com>
2018-09-23 15:27:58 -0400

    initial commit

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,6 +1,7 @@
   <!-- Basic Page Needs -->
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title')</title>
   <meta name="description" content="@yield('dscr')">
   <meta name="keywords" content="@yield('keywords')">
@@ -52,5 +53,3 @@
 
     gtag('config', 'UA-126213276-1');
   </script>
-
-  
