527ccd6ae37083466009997f441ac9336f700093
Taras <taraskim@wknown.com>
2018-09-24 12:57:56 -0400

    initial commit

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
          accessToken: '248274392.1677ed0.8a29b5a11e5f4e0f8b8e6ddd0933ac4c',
          sortBy: 'most-recent',
          limit: 12,
-         template: '<div class="image-item col-lg-2 col-md-2 col-sm-2 col-xs-2 col-sp-4"><a href="{{link}}" target="_blank"><img src="{{model.images.standard_resolution.url}}" {{square}} class="img-in"/></a></div>',
+         template: '<div class="image-item col-lg-2 col-md-2 col-sm-2 col-xs-2 col-sp-12"><a href="{{link}}" target="_blank"><img src="{{model.images.standard_resolution.url}}" {{square}} class="img-in"/></a></div>',
      });
      feed.run();
 });
