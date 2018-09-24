6dac2c5ba3e0c1fe54bdef06000cd5b5507b59d0
Taras <taraskim@wknown.com>
2018-09-24 12:56:59 -0400

    initial commit

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
  var feed = new Instafeed({
          get: 'user',
-         userId: '277022847',
-         clientId: '474194880b8845819aba26a07dc122fd',
-         accessToken: '0fd4a1af09fd4b31a526a10316b1bf55',
+         userId: '248274392',
+         clientId: '4474194880b8845819aba26a07dc122fd',
+         accessToken: '248274392.1677ed0.8a29b5a11e5f4e0f8b8e6ddd0933ac4c',
          sortBy: 'most-recent',
          limit: 12,
-         template: '<div class="image-item col-lg-2 col-md-2 col-sm-2 col-xs-2 col-sp-4"><a href="{{link}}" target="_blank"><img src="{{model.images.standard_resolution.url}}" {{square}} class="img-responsive"/></a></div>',
+         template: '<div class="image-item col-lg-2 col-md-2 col-sm-2 col-xs-2 col-sp-4"><a href="{{link}}" target="_blank"><img src="{{model.images.standard_resolution.url}}" {{square}} class="img-in"/></a></div>',
      });
      feed.run();
 });
