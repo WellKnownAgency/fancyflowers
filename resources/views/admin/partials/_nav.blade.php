0afcea5797188539640857ca3feeae39cf91c87e
Taras <taraskim@wknown.com>
2018-09-27 15:19:58 -0400

    Fix

--- resources/views/admin/partials/_nav.blade.php
+++ resources/views/admin/partials/_nav.blade.php
@@ -1,20 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Admin Fancy Flowers</a>
+  <a class="navbar-brand" href="/admin">Admin Fancy Flowers</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item ">
+        <a class="nav-link" href="/admin">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/admin/products">
           Products
         </a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ Request::is('/admin/collections') ?"active" : ""}}">
         <a class="nav-link" href="/admin/collections">
           Collections
         </a>
