7e1e316d0188c040c631d1a8ace5a2cc9c2f14be
Taras <taraskim@wknown.com>
2018-11-13 10:49:16 -0500

    initial commit

--- public/js/custom.js
+++ public/js/custom.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
          clientId: '05ddbb943b3b4b60b5e174062b6df32e',
          accessToken: '8513871774.1677ed0.db1bb0a0b4f440c9819d89c3afb3bfff',
          sortBy: 'most-recent',
-         limit: 12,
+         limit: 10,
          template: '<div class="image-item col-lg-2 col-md-2 col-sm-2 col-xs-2 col-sp-12"><a href="{{link}}" target="_blank"><img src="{{model.images.standard_resolution.url}}" {{square}} class="img-in"/></a></div>',
      });
      feed.run();
