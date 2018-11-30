61940260bf39ff73a2c4c0a1ec76bf40df17a36f
Taras <taraskim@wknown.com>
2018-11-30 11:43:25 -0500

    initial commit

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -76,7 +76,7 @@
             var myLatLng = {lat: 40.275060, lng: -75.180279};
 
             var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 10,
+                zoom: 8,
                 center: myLatLng
             });
 
